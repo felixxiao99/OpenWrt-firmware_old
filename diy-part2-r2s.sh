5ce498a8da53719c80199a1df4e6c6db51a36329
felixxiao99 <50790338+felixxiao99@users.noreply.github.com>
2021-11-01 20:41:32 +0800

    Update diy-part2-r2s.sh

--- diy-part2-r2s.sh
+++ diy-part2-r2s.sh
@@ -11,14 +11,14 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.9.9.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.9.9.2/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.9/g' package/base-files/files/bin/config_generate
 
 # Modify version info
 sed -i "s/\(DISTRIB_REVISION='\(\w\|\.\)\+\)'/\1 Compiled by xiaofei'/" package/lean/default-settings/files/zzz-default-settings
 
 # swap wan and lan for NanoPi R2S
-#sed -i "s/ucidef_set_interfaces_lan_wan 'eth1' 'eth0'/ucidef_set_interfaces_lan_wan 'eth0' 'eth1'/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s/ucidef_set_interfaces_lan_wan 'eth1' 'eth0'/ucidef_set_interfaces_lan_wan 'eth0' 'eth1'/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 
 # 5.4->5/10
