2e3c2370451a4dd52e812df91781da71252d1b5c
felixxiao99 <50790338+felixxiao99@users.noreply.github.com>
2022-01-08 17:26:28 +0800

    Update diy-part2-x86_64.sh

--- diy-part2-x86_64.sh
+++ diy-part2-x86_64.sh
@@ -18,4 +18,4 @@ sed -i 's/192.168.1.1/10.5.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/\(DISTRIB_REVISION='\(\w\|\.\)\+\)'/\1 Compiled by xiaofei'/" package/lean/default-settings/files/zzz-default-settings
 
 5.10->5.4
-sed -i 's/5.10/5.4/g' target/linux/x86/Makefile
+#sed -i 's/5.10/5.4/g' target/linux/x86/Makefile
