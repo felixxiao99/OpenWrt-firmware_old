a2681603d40102e96734819a1d8a53d7daa7d262
felixxiao99 <50790338+felixxiao99@users.noreply.github.com>
2022-06-19 12:57:46 +0800

    Update diy-part2-r2s.sh

--- diy-part2-r2s.sh
+++ diy-part2-r2s.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/10.9.9.2/g' package/base-files/files/bin/config_generate
 # Modify version info
 sed -i "s/\(DISTRIB_REVISION='\(\w\|\.\)\+\)'/\1 Compiled by xiaofei'/" package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/replace_music_source.rmempty/-- replace_music_source.rmempty/g' feeds/luci/applications/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic/unblockmusic.lua
+# sed -i 's/replace_music_source.rmempty/-- replace_music_source.rmempty/g' feeds/luci/applications/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic/unblockmusic.lua
 
 # swap wan and lan for NanoPi R2S
 #sed -i "s/ucidef_set_interfaces_lan_wan 'eth1' 'eth0'/ucidef_set_interfaces_lan_wan 'eth0' 'eth1'/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
