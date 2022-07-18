44960fd170a34aa661a21a6e41716df6e2a12f2e
felixxiao99 <50790338+felixxiao99@users.noreply.github.com>
2022-07-18 12:00:27 +0800

    Update diy-part2-r2s.sh

--- diy-part2-r2s.sh
+++ diy-part2-r2s.sh
@@ -24,4 +24,4 @@ sed -i 's/replace_music_source.rmempty/-- replace_music_source.rmempty/g' feeds/
 
 
 # 5.4->5/10
-sed -i 's/5.18/5.15/g' target/linux/x86/Makefile
+#sed -i 's/5.18/5.4/g' target/linux/x86/Makefile
